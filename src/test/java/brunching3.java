644755cf6676aa7a9298651767962a8b735025c7
AleksandarBacko <backo.backovic94@gmail.com>
2021-05-25 16:23:58 -0700

    try commit

--- src/test/java/brunching3.java
+++ src/test/java/brunching3.java
@@ -2,5 +2,6 @@ public class brunching3 {
     public static void main(String[] args) {
         System.out.println("brunching 3 ");
         System.out.println("brunching 3 ");
+        System.out.println("sdasdas");
     }
 }
