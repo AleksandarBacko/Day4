d3569d3cf6c72820bdd4e6c82635f014bee954b3
AleksandarBacko <backo.backovic94@gmail.com>
2021-05-25 16:21:19 -0700

    3commit

--- src/test/java/bunching2.java
+++ src/test/java/bunching2.java
@@ -2,5 +2,6 @@ public class bunching2 {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("new brunch ");
+        System.out.println("dsadasd");
     }
 }
